07e3e5a6e10aedede4aa07a57e2fc1dc8a187c59
Deepanshu Jaiswal <deepanshu.jaiswal2025@vitstudent.ac.in>
2026-06-16 17:15:41 +0530

    create server using express

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 // import os from 'os';
 // console.log(`Running on ${os.platform()} platform`);
-import http from 'http';
 
-const server = http.createServer((req, res) =>{
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('This is a Node.js server');
-})
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+
+// import http from 'http';
+
+// const server = http.createServer((req, res) =>{
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('This is a Node.js server');
+// })
+
+// server.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// })
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, this is an Express server!');
+});
+
+app.listen(3000, () => {
+    console.log('Express server is running on port 3000');
+});
