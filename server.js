6b0bdd3a111da30337963c594d45311afce2ef19
Deepanshu Jaiswal <deepanshu.jaiswal2025@vitstudent.ac.in>
2026-06-16 17:11:34 +0530

    create a server

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-import os from 'os';
-console.log(`Running on ${os.platform()} platform`);
+// import os from 'os';
+// console.log(`Running on ${os.platform()} platform`);
+import http from 'http';
+
+const server = http.createServer((req, res) =>{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('This is a Node.js server');
+})
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
