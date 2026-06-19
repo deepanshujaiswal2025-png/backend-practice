18f0d24a803aedccdfae69d5a632c3481adab285
Deepanshu Jaiswal <deepanshu.jaiswal2025@vitstudent.ac.in>
2026-06-19 17:20:27 +0530

    http requests

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.get('/', (req,res)=>{
 })
 
 app.use('/user', router)
+app.use(express.json())
+
+app.post('/users', (req, res) =>{
+    const {name, email} = req.body
+    res.json({
+        message: `User: ${name}\nEmail: ${email}`
+    })
+})
+
+app.put('/users/:id', (req, res)=>{
+    const userID= req.params.id
+    const {name, email} =req.body
+    res.json({
+        message: `User ${userID} updated to ${name}, ${email}`
+    })
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
