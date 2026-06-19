9bedf3639b621c252d4ac194191331195db6f455
Deepanshu Jaiswal <deepanshu.jaiswal2025@vitstudent.ac.in>
2026-06-19 14:04:44 +0530

    express app

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log(process.argv)
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req,res)=>{
+    res.send('Hello, World');
+})
+
+app.get('/about', (req,res) => {
+    res.send('This is the about page');
+})
+
+app.get('/contact', (req, res)=>{
+    res.send('This is contact page')
+})
+
+app.get('/user/:username', (req, res)=>{
+    const username= req.params.username
+    res.send(`Hello, ${username}!`)
+})
+
+app.get('/search', (req, res)=>{
+    const keyword = req.query.keyword;
+    res.send(`Searching for ${keyword}`)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
