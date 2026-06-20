5762d3c0df3a66eb30bc922b8be74f6f22d5fb3b
Deepanshu Jaiswal <deepanshu.jaiswal2025@vitstudent.ac.in>
2026-06-20 18:02:38 +0530

    POST request

--- app.js
+++ app.js
@@ -4,25 +4,22 @@ import router from './router.js'
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req,res)=>{
-    res.send('Hello, World');
-})
-
-app.use('/user', router)
 app.use(express.json())
 
-app.post('/users', (req, res) =>{
-    const {name, email} = req.body
-    res.json({
-        message: `User: ${name}\nEmail: ${email}`
-    })
+app.get('/', (req,res)=>{
+    res.send({
+        message:'Hello, World',
+        name: "deepanshu",
+        error: "no error"
+    });
 })
 
-app.put('/users/:id', (req, res)=>{
-    const userID= req.params.id
-    const {name, email} =req.body
-    res.json({
-        message: `User ${userID} updated to ${name}, ${email}`
+app.post('/', (req, res)=>{
+    const data= req.body
+    console.log(data.name)
+    res.send({
+        message: "data received", 
+        data
     })
 })
 
