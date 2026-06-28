d89c3957b559dca6fa48b826400c699babb39050
Deepanshu Jaiswal <deepanshu.jaiswal2025@vitstudent.ac.in>
2026-06-28 17:19:56 +0530

    file module

--- file.cjs
+++ file.cjs
@@ -5,4 +5,14 @@ fs.mkdir(path.join(__dirname, "html",), {}, (err) => {
     console.log('Directory created');
 })
 
+fs.writeFile(path.join(__dirname, "html", "sample.txt"), "hello world", (err) => {
+    console.log('Written');
+})
 
+fs.appendFile(path.join(__dirname, "html", "sample.txt"), "\nhello world 2", (err) => {
+    console.log('Written');
+})
+
+fs.readFile(path.join(__dirname, "html", "sample.txt"), "utf8", (err, content) => {
+    console.log(content);
+})
